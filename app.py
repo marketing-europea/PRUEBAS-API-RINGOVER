d2bd629454bd2d101583828acb9d929d419d85a1
marketing-europea <comunicacion@europeaseguros.es>
2026-06-12 10:23:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
+import streamlit as st
 import requests
 
 API_KEY = "9100d3646e618b7526417ada74853f620bcfa288"
 
+st.title("Prueba Ringover")
+
 r = requests.get(
     "https://public-api.ringover.com/v2/calls",
-    headers={
-        "Authorization": API_KEY
-    },
-    params={
-        "limit_count": 1
-    }
+    headers={"Authorization": API_KEY},
+    params={"limit_count": 1}
 )
 
-print("STATUS:", r.status_code)
-print("CONTENT TYPE:", r.headers.get("content-type"))
-print("RESPUESTA:")
-print(r.text[:5000])
+st.write("STATUS:", r.status_code)
+st.write("CONTENT TYPE:", r.headers.get("content-type"))
+st.text(r.text[:5000])
